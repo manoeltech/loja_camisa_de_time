a3a4e4c1952a4569b971ad83a6e6e6e631beb9f3
manoeltech <manoel.programador21@gmail.com>
2024-03-16 19:54:00 -0300

    Create translate

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module LojaCamisaDeTime
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = :'pt-BR'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
