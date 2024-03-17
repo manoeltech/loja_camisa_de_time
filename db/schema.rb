30f68c0055027df5c2c571a4337f244bb1a44f08
manoeltech <manoel.programador21@gmail.com>
2024-03-17 11:50:14 -0300

    Include gem devise in project and remove model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_16_222451) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_17_143720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,28 +41,27 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_16_222451) do
     t.text "description"
     t.integer "quantify"
     t.bigint "company_id", null: false
-    t.bigint "user_id", null: false
     t.bigint "client_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "status", default: true
     t.index ["client_id"], name: "index_products_on_client_id"
     t.index ["company_id"], name: "index_products_on_company_id"
-    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.bigint "company_id", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["company_id"], name: "index_users_on_company_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "clients", "companies"
   add_foreign_key "products", "clients"
   add_foreign_key "products", "companies"
-  add_foreign_key "products", "users"
-  add_foreign_key "users", "companies"
 end
